302a8cb260cc4a47ae7d33c8ef9ca201e2f9babe
tishtiaq <140320788+tishtiaq@users.noreply.github.com>
2026-07-08 17:01:38 +0100

    changes to baselevel fitness

--- src/CCM/MoranLinearFitness.py
+++ src/CCM/MoranLinearFitness.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 from clone_competition_simulation import (Moran, NonSpatialCurrentData, Parameters, PopulationParameters, 
                                           TimeParameters, FitnessParameters)
-
+from clone_competition_simulation.simulation_algorithms import current_data
 
 class MoranLinearFitness(Moran):
 
@@ -22,18 +22,20 @@ class MoranLinearFitness(Moran):
         self.intercept = a_intercept
         # Defines the gradient and intercept
         super().__init__(parameters)
-    
+        for i, fitness in enumerate(parameters.fitness.initial_fitness_array):
+            self.clones_array[i, self.fitness_idx] = fitness
+
 
     def get_dividing_cell(self, current_data: NonSpatialCurrentData) -> int:     # This function determines which cell will divide
 # Returns the index of the cell that has highest fitness in the current_data.current_population array.
 
         current_time = self.i / self.division_rate
-        new_fitness = max(self.intercept - self.slope * current_time, 0.01)   
+        new_fitness = max(self.intercept - self.slope * current_time, 1.01)   
         # Here we define the operation, and ensure fitness doesn't go below 0.01 to avoid negative fitness values
         self.clones_array[1, self.fitness_idx] = new_fitness
-
+        
         return super().get_dividing_cell(current_data)
-
+        
 '''
     def get_differentiating_cell(self, current_data: NonSpatialCurrentData) -> int:  # This function determines which cell will die
         return 0
@@ -43,8 +45,8 @@ class MoranLinearFitness(Moran):
 
 params = Parameters(
     algorithm="Moran", times = TimeParameters(max_time=10, division_rate=1, samples=6),
-    population=PopulationParameters(initial_size_array=np.array([5000,5000])),  # Change ratio of wild-type : higher fitness HERE
-    fitness = FitnessParameters(initial_fitness_array=np.array([1, 1.3]))       # Change fitness of fitter clone HERE
+    population=PopulationParameters(initial_size_array=np.array([5000,5000])),  # Change ratio of wild-type : higher fitness HERE 
+    fitness = FitnessParameters(initial_fitness_array=np.array([1, 1.5]))       # Change fitness of fitter clone HERE (and see line 55)
     )
 
 
@@ -52,8 +54,9 @@ import os
 docs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs')
 
 
-sim_linear = MoranLinearFitness(params, a_intercept=1.3)
+sim_linear = MoranLinearFitness(params, a_intercept=1.5)  # Need to change the fitness HERE as well 
 sim_linear.run_sim()
+
 
 sim_constant = Moran(params)
 sim_constant.run_sim()
@@ -66,7 +69,7 @@ ax1.set_title("Linearly Decreasing Fitness")
 sim_constant.muller_plot(ax=ax2) # Plot the constant simulation on the other side
 ax2.set_title("Constant Fitness")
 fig1.suptitle("Muller Plots")
-plt.savefig(os.path.join(docs_dir, '50percent_1.3_muller_comparison_moran.png'), dpi=150, bbox_inches='tight')
+# plt.savefig(os.path.join(docs_dir, '10percent_1.05_muller_comparison_moran.png'), dpi=150, bbox_inches='tight')
 
 
 # Plot Mean Clone Sizes
@@ -76,7 +79,7 @@ ax3.set_title("Linearly Decreasing Fitness")
 sim_constant.plot_mean_clone_size_graph_for_non_mutation(ax=ax4)
 ax4.set_title("Constant Fitness")
 fig2.suptitle("Mean Clone Size")
-plt.savefig(os.path.join(docs_dir, '50percent_1.3_size_comparison_moran.png'), dpi=150, bbox_inches='tight')
+# plt.savefig(os.path.join(docs_dir, '10percent_1.05_size_comparison_moran.png'), dpi=150, bbox_inches='tight')
 
 
 # Plot Survival Rate on the same graph 
@@ -86,7 +89,7 @@ sim_linear.plot_surviving_clones_for_non_mutation(ax=ax, legend_label='Linearly 
 ax.legend()
 plt.title("Survival Comparison: Constant vs Linearly Decreasing Fitness")
 plt.tight_layout
-plt.savefig(os.path.join(docs_dir, '50percent_1.3_survival_comparison_moran.png'), dpi=150, bbox_inches='tight')
+# plt.savefig(os.path.join(docs_dir, '10percent_1.05_survival_comparison_moran.png'), dpi=150, bbox_inches='tight')
 
 
 plt.show()
