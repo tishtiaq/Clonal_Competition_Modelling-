51c11107226ddbf5e6d314dd66512f55b46aab48
tishtiaq <140320788+tishtiaq@users.noreply.github.com>
2026-07-08 15:13:41 +0100

    Added plotting functions at bottom for comparisons'

--- src/CCM/MoranLinearFitness.py
+++ src/CCM/MoranLinearFitness.py
@@ -2,9 +2,13 @@
 
 # Making custom Moran (Non-Spatial)
 
+# I have updated the bottom of the code so that it now plots comparison plots between simulations which uses the linear decay model and simulations which use the old model.
+# The plots show the difference in muller plots, mean clone size and survival rate of clones. You can easily adapt population sizes, ratio of wild-type : higher fitness clones, 
+# and starting fitness values by changing the stored floats in the code where labelled. 
+
+
 import numpy as np 
 import matplotlib.pyplot as plt
-import pandas as pd
 
 from clone_competition_simulation import (Moran, NonSpatialCurrentData, Parameters, PopulationParameters, 
                                           TimeParameters, FitnessParameters)
@@ -34,28 +38,55 @@ class MoranLinearFitness(Moran):
     def get_differentiating_cell(self, current_data: NonSpatialCurrentData) -> int:  # This function determines which cell will die
         return 0
 '''        
-# We are commenting this out because we want to keep the parent class' code for this: we want cell death to be random
+# We are commenting this out because we want to keep the parent class' code for this: we want cell death to be random. In the Moran model, usually 
+# you need to overwrite 2 functions. 
 
 params = Parameters(
     algorithm="Moran", times = TimeParameters(max_time=10, division_rate=1, samples=6),
-    population=PopulationParameters(initial_size_array=np.array([500,500])),
-    fitness = FitnessParameters(initial_fitness_array=np.array([1, 1.5]))
+    population=PopulationParameters(initial_size_array=np.array([5000,5000])),  # Change ratio of wild-type : higher fitness HERE
+    fitness = FitnessParameters(initial_fitness_array=np.array([1, 1.3]))       # Change fitness of fitter clone HERE
     )
-
-sim = MoranLinearFitness(params, a_intercept=1.5)
-
-sim.run_sim()
-sim.muller_plot(figsize=(5, 5))
-plt.title("Moran Simulation Between One Wild-Type Clone and One Fitter Clone of Linearly Decreasing Fitness")
-plt.xlabel("Time")
-plt.ylabel("Clone Size")
-
-print(sim.population_array.toarray())
 
 
 import os
 docs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs')
-plt.savefig(os.path.join(docs_dir, 'moran_muller_linear_500_500.png'), dpi=150, bbox_inches='tight')
-# Saves the file under name moran (make sure to get rid of the MullerPlotsMoran.md folder and these aren't actual Moran simulations)
+
+
+sim_linear = MoranLinearFitness(params, a_intercept=1.3)
+sim_linear.run_sim()
+
+sim_constant = Moran(params)
+sim_constant.run_sim()
+
+
+# Below plots the Muller Plots side by side
+fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+sim_linear.muller_plot(ax=ax1) # Plot the linear simulation on one side
+ax1.set_title("Linearly Decreasing Fitness")
+sim_constant.muller_plot(ax=ax2) # Plot the constant simulation on the other side
+ax2.set_title("Constant Fitness")
+fig1.suptitle("Muller Plots")
+plt.savefig(os.path.join(docs_dir, '50percent_1.3_muller_comparison_moran.png'), dpi=150, bbox_inches='tight')
+
+
+# Plot Mean Clone Sizes
+fig2, (ax3, ax4) = plt.subplots(1, 2, figsize=(12, 5))
+sim_linear.plot_mean_clone_size_graph_for_non_mutation(ax=ax3)
+ax3.set_title("Linearly Decreasing Fitness")
+sim_constant.plot_mean_clone_size_graph_for_non_mutation(ax=ax4)
+ax4.set_title("Constant Fitness")
+fig2.suptitle("Mean Clone Size")
+plt.savefig(os.path.join(docs_dir, '50percent_1.3_size_comparison_moran.png'), dpi=150, bbox_inches='tight')
+
+
+# Plot Survival Rate on the same graph 
+fig, ax = plt.subplots(figsize=(8, 5))  # creates 1 figure with two graphs
+sim_constant.plot_surviving_clones_for_non_mutation(ax=ax, legend_label='Constant Fitness')
+sim_linear.plot_surviving_clones_for_non_mutation(ax=ax, legend_label='Linearly Decreasing Fitness') 
+ax.legend()
+plt.title("Survival Comparison: Constant vs Linearly Decreasing Fitness")
+plt.tight_layout
+plt.savefig(os.path.join(docs_dir, '50percent_1.3_survival_comparison_moran.png'), dpi=150, bbox_inches='tight')
+
 
 plt.show()
