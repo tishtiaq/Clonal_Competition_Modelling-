20896d59b4b7ba2d4a960b68daaa47a63897aa31
tishtiaq <140320788+tishtiaq@users.noreply.github.com>
2026-07-10 15:02:17 +0100

    fix bug where all fitnesses where set to 0

--- src/CCM/WF2DLinearFitness.py
+++ src/CCM/WF2DLinearFitness.py
@@ -16,37 +16,39 @@ class WFLinearFitness2D(WF2D):
         self.intercept = a_intercept
         # Defines the gradient and intercept
         super().__init__(parameters)
-# Same linear function defined above. 
+        for i, fitness in enumerate(parameters.fitness.initial_fitness_array):
+            self.clones_array[i, self.fitness_idx] = fitness
+    # Same linear function defined above. 
 
     def get_next_generation(self, current_data: SpatialCurrentData) -> np.ndarray[tuple[int], np.dtype[np.int_]]:
         current_time = self.i / self.division_rate
         new_fitness = self.intercept - self.slope * current_time   # Here we define the operation
         self.clones_array[1, self.fitness_idx] = new_fitness
         return super().get_next_generation(current_data) 
-    
-initial_grid = np.zeros((50,50), dtype=int) 
-initial_grid[:37, :] = 1      # 2D slicing syntax: first colon is rows, second is columns
+if __name__ == "__main__":    
+    initial_grid = np.zeros((50,50), dtype=int) 
+    initial_grid[:37, :] = 1      # 2D slicing syntax: first colon is rows, second is columns
 
-params = Parameters(
-    algorithm="WF2D", times= TimeParameters(max_time=100, division_rate=1, samples=4),
-    population= PopulationParameters(initial_grid=initial_grid, cell_in_own_neighbourhood=False),
-    fitness= FitnessParameters(initial_fitness_array=np.array([1, 1.5])),
-)
-        
+    params = Parameters(
+        algorithm="WF2D", times= TimeParameters(max_time=100, division_rate=1, samples=4),
+        population= PopulationParameters(initial_grid=initial_grid, cell_in_own_neighbourhood=False),
+        fitness= FitnessParameters(initial_fitness_array=np.array([1, 1.5])),
+    )
+            
 
-sim = WFLinearFitness2D(params, a_intercept=1.5)
-sim.run_sim()
-sim.muller_plot(figsize=(5, 5))
+    sim = WFLinearFitness2D(params, a_intercept=1.5)
+    sim.run_sim()
+    sim.muller_plot(figsize=(5, 5))
 
-plt.title("Competition Between One Wild-Type Clone and One Fitter Clone of Linearly Decreasing Fitness")
-plt.xlabel("Time")
-plt.ylabel("Clone Size")
+    plt.title("Competition Between One Wild-Type Clone and One Fitter Clone of Linearly Decreasing Fitness")
+    plt.xlabel("Time")
+    plt.ylabel("Clone Size")
 
-import os
-docs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs')
-plt.savefig(os.path.join(docs_dir, 'f_wf_2D_linear_75_25.png'), dpi=150, bbox_inches='tight')
+    import os
+    docs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs')
+    plt.savefig(os.path.join(docs_dir, 'f_wf_2D_linear_75_25.png'), dpi=150, bbox_inches='tight')
 
-plt.show()
+    plt.show()
 
 
 
